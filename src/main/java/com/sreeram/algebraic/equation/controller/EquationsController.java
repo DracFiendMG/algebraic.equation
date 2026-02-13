79c96920000000d40b0fc7fa6e4c43dd86f483be
DracFiendMG <sreeramrdy@gmail.com>
2026-02-13 18:44:10 +0530

    ready with the blueprint of the controller

--- src/main/java/com/sreeram/algebraic/equation/controller/EquationsController.java
+++ src/main/java/com/sreeram/algebraic/equation/controller/EquationsController.java
@@ -1,13 +1,34 @@
 package com.sreeram.algebraic.equation.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sreeram.algebraic.equation.model.EquationRequest;
+import com.sreeram.algebraic.equation.model.EquationResponse;
+import com.sreeram.algebraic.equation.model.EvaluationResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/equations")
 public class EquationsController {
-    public ResponseEntity<String> storeEquation() {
-        return ResponseEntity.ok("Equation stored successfully");
+
+    @PostMapping("/store")
+    public EquationResponse storeEquation(
+            @RequestBody EquationRequest request
+    ) {
+        return new EquationResponse();
+    }
+
+    @GetMapping
+    public List<EquationResponse> getAllEquations(
+    ) {
+        return List.of();
+    }
+
+    @PostMapping("/{equationId}/evaluate")
+    public EvaluationResponse evaluateEquation(
+            @PathVariable Long equationId,
+            @RequestBody EquationRequest request
+    ) {
+        return new EvaluationResponse();
     }
 }
